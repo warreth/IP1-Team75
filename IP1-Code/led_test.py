b5fa78a2e57a4cbd87c42ab416bc7879c7469bbf
WaTh <info@wath.dev>
2025-10-29 15:25:16 +0100

    Update led_test.py

--- IP1-Code/led_test.py
+++ IP1-Code/led_test.py
@@ -1,31 +1,15 @@
-from machine import Pin
+import machine
 import time
 
-led_ch1 = Pin(10, Pin.OUT)
-led_ch2 = Pin(11, Pin.OUT)
-led_ch3 = Pin(12, Pin.OUT)
+led_ch1 = machine.PWM(machine.Pin(2))
+led_ch2 = machine.PWM(machine.Pin(3))
+led_ch3 = machine.PWM(machine.Pin(12))
 
-x = 0.5
 
-led_ch1.value(0)
-led_ch2.value(0)
-led_ch3.value(0)
 
-while True:
-    # CH1 aan, rest uit
-    led_ch1.value(1)
-    led_ch2.value(0)
-    led_ch3.value(0)
-    time.sleep(x)
-    
-    # CH2 aan, rest uit
-    led_ch1.value(0)
-    led_ch2.value(1)
-    led_ch3.value(0)
-    time.sleep(x)
-    
-    # CH3 aan, rest uit
-    led_ch1.value(0)
-    led_ch2.value(0)
-    led_ch3.value(1)
-    time.sleep(x)
+def set_led_brightness(led, brightness):
+    led.duty_u16(brightness)
+
+
+
+set_led_brightness(led_ch1, 60000)
